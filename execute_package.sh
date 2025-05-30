ddc3c58463d091b451a35d6eff35b577552324b5
ShawdoEvan <mnbhjk3@gmail.com>
2025-05-30 10:14:48 +0800

    Update to 1.0.1

--- execute_package.sh
+++ execute_package.sh
@@ -5,5 +5,5 @@ docker build \
   --build-arg=PYTHON_VERSION=3.11.9 \
   --build-arg=NODE_VERSION=18.20.2 \
   --build-arg=APPS_JSON_BASE64=$APPS_JSON_BASE64 \
-  --tag=mnbhjk2/joinfun_erpnext:1.0.0 \
+  --tag=mnbhjk2/joinfun_erpnext:1.0.1 \
   --file=images/production/Containerfile .
